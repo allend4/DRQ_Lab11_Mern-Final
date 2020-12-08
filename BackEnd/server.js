ed728d571edb3ea60705d151fdcdb0e8faeed23b
allend4 <g00375372@gmit.ie>
2020-12-08 11:44:02 +0000

    final lab update + comments

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 4000 // changed as react uses 3000
-const cors = require('cors'); // cors need installing and will allow script to request data off server
+//const cors = require('cors'); // cors need installing and will allow script to request data off server - NOT NEEDED ANYMORE
 const bodyParser = require("body-parser") // // body-parser is a piece of express middleware that reads a form's input and stores it as a javascript object accessible through req.bod
 const mongoose = require('mongoose'); // getting-started.js mongoose
+const path = require('path') // provides utilities for working with file and directory paths
 
-// cors
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// cors - one application so becomes reduntant - two domains not needed
+//app.use(cors());
+//app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//    res.header("Access-Control-Allow-Headers",
+//        "Origin, X-Requested-With, Content-Type, Accept");
+//    next();
+//});
+
+// build and static folder configurations
+app.use(express.static(path.join(__dirname, '../build'))); // config - where to find files (current diretory => build folder)
+app.use('/static', express.static(path.join(__dirname, 'build/static'))); // (current diretory => static folder)
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -81,6 +86,10 @@ app.post('/api/movies', (req, res) => { // POST method
         res.send('item created')
     })
     .catch();
+})
+
+app.get('*', (req, res) => { // '*' all other routes
+    res.sendFile(path.join(__dirname + '/../build/index.html')); // send file back (directory => build/index.html)
 })
 
 app.listen(port, () => { // app listen at port 4000. used to bind and listen the connections on the specified host and port
